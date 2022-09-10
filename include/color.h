904b64b0c01fe3ec42c0624af9d28382eac7b2ba
raian621 <ryan.bell62101@gmail.com>
2022-09-09 20:06:55 -0500

    created color class.

---
+++ include/color.h
@@ -0,0 +1,34 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include <iostream>
+#include <stdint.h>
+
+struct color
+{
+  float r, g, b;
+
+  color(float r = 0, float g = 0, float b = 0);
+
+  uint32_t int_rgb() const;
+
+  color operator+(const color&) const;
+  color operator-(const color&) const;
+  color operator-() const;
+  color operator*(float) const;
+  color operator/(float) const;
+
+  color& operator+=(const color&);
+  color& operator-=(const color&);
+  color& operator*=(float);
+  color& operator/=(float);
+
+  bool operator==(const color&) const;
+};
+
+color clamp(const color&) noexcept;
+float dot_product(const color&, const color&) noexcept;
+color cross_product(const color&, const color&) noexcept;
+std::ostream& operator<<(std::ostream&, const color&);
+color operator*(float, const color&);
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
-#include "vector/vec3.h"
+#include "color.h"
+#include "vector/vec2.h"
 #include "canvas.h"
 
 int main()
 {
-  Canvas canvas(30, 30);
-  for (int x = 0; x < canvas.width(); x++)
-    for (int y = 0; y < canvas.width(); y++)
-      canvas[x][y] = 0x745400;
+  color c1(1, 0.5, 0.25 );
+  color c2 = clamp(c1 * 2);
+  color c3(0, 0, 0);
+  Canvas canvas(300, 300);
 
-  for (int x = 0; x < canvas.width(); x++)
-    canvas[x][10] = 0xffffff;
+  vec2 center = { canvas.width() / 2, canvas.width() / 2 };
+  vec2 coord;
+  float radius = 100.0f;
+
+  for(coord.x = 0; coord.x < canvas.width(); coord.x++)
+    for (coord.y = 0; coord.y < canvas.height(); coord.y++)
+      canvas[(int)coord.x][(int)coord.y] = c1.int_rgb();
+
+  for(coord.x = 0; coord.x < canvas.width(); coord.x++)
+    for (coord.y = 0; coord.y < canvas.height(); coord.y++)
+      if ((center - coord).magnitude() <= radius)
+        canvas[(int)coord.x][(int)coord.y] = c3.int_rgb();
+      else
+        if ((int)coord.y / 10 % 2)
+          canvas[(int)coord.x][(int)coord.y] = c2.int_rgb();
+        else if ((int)coord.x / 25 % 2)
+          canvas[(int)coord.x][(int)coord.y] = c2.int_rgb() / 2;
 
   canvas.save_to_file("../out", "plain.ppm");
 }
